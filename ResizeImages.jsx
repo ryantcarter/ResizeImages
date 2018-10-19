1928253c4d4c21475bf0d258fbfe2dd9969365cd
Charles Harries <charlesgharries@gmail.com>
2018-10-19 11:36:26 +0100

    Change `var` for `const` and fix indentation

--- ResizeImages.jsx
+++ ResizeImages.jsx
@@ -13,94 +13,86 @@ const docRes = 72; // Picture resolution in pixels
 //PROGRAM AREA - USER SHOULD NOT CHANGE ANYTHING BELOW THIS 
 /////////////////////////////////////////////////////////////////////////////// 
 //assign variables 
-var startRulerUnits; 
-var startTypeUnits; 
-var startDisplayDialogs; 
+const startRulerUnits; 
+const startTypeUnits; 
+const startDisplayDialogs; 
 
-var downSizing = false 
+const downSizing = false 
 
 /////////////////////////////////////////////////////////////////////////////// 
 // Dispatch 
 /////////////////////////////////////////////////////////////////////////////// 
 try { 
-main(); 
+  main(); 
 
 /////////////////////////////////////////////////////////////////////////////// 
 // Functions 
 /////////////////////////////////////////////////////////////////////////////// 
-function main() 
-{ 
-//collect dialog defaults so that the program can set them back afterwards 
-startDisplayDialogs = displayDialogs; 
-startRulerUnits = preferences.rulerUnits; 
-startTypeUnits = preferences.typeUnits; 
+  function main()  { 
+    //collect dialog defaults so that the program can set them back afterwards 
+    conststartDisplayDialogs = displayDialogs; 
+    startRulerUnits = preferences.rulerUnits; 
+    startTypeUnits = preferences.typeUnits; 
 
-//set dialogs 
-displayDialogs = DialogModes.NO; 
-preferences.rulerUnits = Units.PIXELS; 
-preferences.typeUnits = TypeUnits.PIXELS; 
+    //set dialogs 
+    displayDialogs = DialogModes.NO; 
+    preferences.rulerUnits = Units.PIXELS; 
+    preferences.typeUnits = TypeUnits.PIXELS; 
 
-//collect the height, width & resolution of the active document 
-var activeDoc = app.activeDocument; 
-var docHeight= activeDoc.height; 
-var docWidth = activeDoc.width; 
-//var docRes = activeDoc.resolution 
-var docRatio = docWidth / docHeight; 
+    //collect the height, width & resolution of the active document 
+    const activeDoc = app.activeDocument; 
+    const docHeight= activeDoc.height; 
+    const docWidth = activeDoc.width; 
+    const docRatio = docWidth / docHeight; 
 
-if (docHeight > docWidth) { 
-docRatio = docHeight / docWidth; 
-newHeight = targetPixels; 
-newWidth = ((1.0 * newHeight) / docRatio) 
-newWidth = Math.round(newWidth); // make integer 
-//sizing up or down? 
-if (docHeight > targetPixels) { 
-downSizing = true 
-} 
-} 
-else { 
-docRatio = docWidth / docHeight; 
-newWidth = targetPixels; 
-newHeight = ((1.0 * newWidth) / docRatio) 
-newHeight = Math.round(newHeight); // make integer 
-//sizing up or down? 
-if (docWidth > targetPixels) { 
-downSizing = true 
-} 
-} 
+    if (docHeight > docWidth) { 
+      docRatio = docHeight / docWidth; 
+      newHeight = targetPixels; 
+      newWidth = ((1.0 * newHeight) / docRatio) 
+      newWidth = Math.round(newWidth); // make integer 
+      //sizing up or down? 
+      if (docHeight > targetPixels) { 
+        downSizing = true 
+      } 
+    } 
+    else { 
+      docRatio = docWidth / docHeight; 
+      newWidth = targetPixels; 
+      newHeight = ((1.0 * newWidth) / docRatio) 
+      newHeight = Math.round(newHeight); // make integer 
+      //sizing up or down? 
+      if (docWidth > targetPixels) { 
+        downSizing = true 
+      } 
+    } 
 
-if (downSizing) { 
-resampleMeth = ResampleMethod.BICUBICSHARPER; 
-} 
-else { 
-resampleMeth = ResampleMethod.BICUBICSMOOTHER; 
-} 
+    if (downSizing) { 
+      resampleMeth = ResampleMethod.BICUBICSHARPER; 
+    } else { 
+      resampleMeth = ResampleMethod.BICUBICSMOOTHER; 
+    } 
 
-activeDoc.resizeImage(newWidth, newHeight, docRes, resampleMeth) 
+    activeDoc.resizeImage(newWidth, newHeight, docRes, resampleMeth) 
 
-//reset preferences to original 
-preferences.rulerUnits = startRulerUnits; 
-preferences.typeUnits = startTypeUnits; 
-displayDialogs = startDisplayDialogs; 
+    //reset preferences to original 
+    preferences.rulerUnits = startRulerUnits; 
+    preferences.typeUnits = startTypeUnits; 
+    displayDialogs = startDisplayDialogs; 
 
-activeDoc = null; 
+    activeDoc = null; 
+  } //end function main 
+} catch(e) { 
+  alert( e ); 
 
-} //end function main 
+  if ( undefined != startDisplayDialogs ) { 
+    displayDialogs = startDisplayDialogs; 
+  } 
 
-} //end trry 
+  if ( undefined != startRulerUnits ) { 
+    preferences.rulerUnits = startRulerUnits; 
+  } 
 
-catch(e) { 
-alert( e ); 
-
-if ( undefined != startDisplayDialogs ) { 
-displayDialogs = startDisplayDialogs; 
-} 
-
-if ( undefined != startRulerUnits ) { 
-preferences.rulerUnits = startRulerUnits; 
-} 
-
-if ( undefined != startTypeUnits ) { 
-preferences.typeUnits = startTypeUnits; 
-} 
-
+  if ( undefined != startTypeUnits ) { 
+    preferences.typeUnits = startTypeUnits; 
+  } 
 } 
